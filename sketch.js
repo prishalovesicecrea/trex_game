5410ce8c800cd08ef0f7511d7596df5e1dfa3483
prishalovesicecrea <86233355+prishalovesicecrea@users.noreply.github.com>
2021-06-21 16:05:36 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -72,7 +72,7 @@ function draw() {
   //trex.debug = true;
   background(255);
   text("Score: "+ score, 500,50);
-  
+  text("prisha's trex game",250,50);
   if (gameState===PLAY){
     score = score + Math.round(getFrameRate()/60);
     ground.velocityX = -(6 + 3*score/100);
